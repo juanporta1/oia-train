c73976841a213f066c0755fc470083050614b5ab
Your Name <you@example.com>
2024-06-14 12:10:43 -0300

    carpetas

---
+++ nivel3/sumando_divisores_nivel3/cadenas3.py
@@ -0,0 +1,38 @@
+def cadenas_divisores(numeros):
+    totales = []
+    for numero in numeros:
+        if num != 0:
+            divisor_actual = numero
+            total = 0
+            total += divisor_actual
+            print(numero)
+            while divisor_actual != 1:
+                count = 2
+                while True:
+                    if divisor_actual % count == 0:
+                        divisor_actual /= count
+                        divisor_actual = int(divisor_actual)
+                        break
+                    else:
+                        count += 1
+                total += divisor_actual
+                print(divisor_actual)
+            totales.append(total)
+    return totales
+
+num = 1
+while True:
+    num = 1
+    numeros = []
+    while num != 0:
+        num = input("Ingrese un numero: ")
+
+        while not num.isdigit():
+            num = input("Ingreselo nuevamente: ")
+        num = int(num)
+        
+        numeros.append(num)
+        if num == 0 or num == -1: break   
+    if num == -1: break
+    
+    print(cadenas_divisores(numeros))
